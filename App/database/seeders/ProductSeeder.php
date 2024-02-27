88a95b797ee5c0d89df62e92a3214c908a0850ec
Ena <ena.fibinger@gmail.com>
2024-02-27 19:27:28 +0000

    seeder, routes and navigaion

---
+++ App/database/seeders/ProductSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('products')->insert(['name' => 'test', 'description' => 'testni proizvod', 'code' => '1234']);
+    }
+}
